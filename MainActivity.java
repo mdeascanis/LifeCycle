5a417d9201a0f1932fba677068f40a4a79892e84
cis282student <mdeascanis@outlook.com>
2017-01-19 13:09:57 -0500

    figured out toast messages

--- MainActivity.java
+++ MainActivity.java
@@ -3,6 +3,8 @@ package edu.dtcc.cis282student.lifecycle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,45 +14,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.v("Matt", "");
+
+        Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
+
     }
+
+
+
 
     @Override
-    protected void onStart(){
-        Log.v("Matt", "");
-
+     protected void onStart() {
+        super.onStart();
+         Log.v("Matt", "");
+        Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
     }
+
+
+
 
     @Override
     protected void onResume(){
+        super.onResume();
         Log.v("Matt", "");
+        Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
     protected void onRestart(){
+        super.onRestart();
         Log.v("Matt", "");
+        Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
     protected void onPause(){
+        super.onPause();
         Log.v("Matt", "");
+        Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
     protected void onStop(){
+        super.onStop();
         Log.v("Matt", "");
+        Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
     protected void onDestroy(){
+        super.onDestroy();
         Log.v("Matt", "");
+        Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
 
     }
-
-
-
 
 }
