91ebc5926f4af971710083d981aa76ec346ef802
cis282student <mdeascanis@outlook.com>
2017-01-19 12:54:20 -0500

    created various methods

--- MainActivity.java
+++ MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.v("Matt", "onCreate has been called");
+        Log.v("Matt", "");
     }
 
     @Override
