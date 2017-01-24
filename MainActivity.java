a08e3aca67249cb8792fcf0562c03c9124c4e7dd
cis282student <mdeascanis@outlook.com>
2017-01-24 12:07:01 -0500

    figured out toast messages

--- MainActivity.java
+++ MainActivity.java
@@ -13,62 +13,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.v("Matt", "");
+        Log.v("Matt", "onCreate");
 
-        Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
 
     }
-
-
 
 
     @Override
      protected void onStart() {
         super.onStart();
-         Log.v("Matt", "");
-        Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
+         Log.v("Matt", "onStart");
+        Toast.makeText(getApplicationContext(), "onStart", Toast.LENGTH_SHORT).show();
     }
-
 
 
 
     @Override
     protected void onResume(){
         super.onResume();
-        Log.v("Matt", "");
-        Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
+        Log.v("Matt", "onREsume");
+        Toast.makeText(getApplicationContext(), "onREsume", Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
     protected void onRestart(){
         super.onRestart();
-        Log.v("Matt", "");
-        Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
+        Log.v("Matt", "onREstart");
+        Toast.makeText(getApplicationContext(), "onREstart", Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
     protected void onPause(){
         super.onPause();
-        Log.v("Matt", "");
-        Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
+        Log.v("Matt", "onPause");
+        Toast.makeText(getApplicationContext(), "onPause", Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
     protected void onStop(){
         super.onStop();
-        Log.v("Matt", "");
-        Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
+        Log.v("Matt", "onStop");
+        Toast.makeText(getApplicationContext(), "onStop", Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
     protected void onDestroy(){
         super.onDestroy();
-        Log.v("Matt", "");
-        Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
+        Log.v("Matt", "onDestroy");
+        Toast.makeText(getApplicationContext(), "onDestroy", Toast.LENGTH_SHORT).show();
 
     }
 
